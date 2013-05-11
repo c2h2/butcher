4a89c6e6a596ee2d1b714865fd241a9a76a3d875
qichunren <whyruby@gmail.com>
2013-05-11 18:59:55 +0800

    update db seed

--- www/db/seeds.rb
+++ www/db/seeds.rb
@@ -1,6 +1,11 @@
 # encoding: utf-8
+start_time = Time.now
 $mongo = true
+
+Word.destroy_all
+
 require File.expand_path("../../../chop.rb", __FILE__)
 
 dict=Dict.new
 dict.load_from_file File.expand_path("../../../dict.txt.big", __FILE__)
+puts "Take #{Time.now - start_time} seconds"
