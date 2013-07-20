b89edf03e82f05bc1fbd444b42dff2bb60a5b74f
Yiling Cao <yiling.cao@gmail.com>
2013-07-20 22:50:02 +0800

    change db seed, able to fit with new dir structure.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,22 @@
 # encoding: utf-8
 start_time = Time.now
 $mongo = true
-db_file = "../../../dicts/dict.txt.big"
-attr_file = "../dicts/words_attr.txt"
+db_file = "dicts/dict.txt.big"
+attr_file = "dicts/words_attr.txt"
 
-puts "Flush old dict db."
+puts "Flushing old dict db."
 Word.destroy_all
-puts "Done, loading dict #{db_file}, Time now = #{start_time}"
-
-require File.expand_path("../../../chop.rb", __FILE__)
+puts "Done flushing old db, loading dict #{db_file}, Time now = #{start_time}"
+require_relative "../app/models/chop.rb"
 dict=Dict.new
-dict.load_from_file File.expand_path(db_file, __FILE__)
+dict.load_from_file db_file
 puts "Done, loaded dict db. Cost #{Time.now - start_time} seconds."
 
+
+start_time = Time.now
+puts "Done adding new words to db, adding attrs, Time now = #{start_time}"
 Attr.destroy_all
 attr=Attr.new
 content=File.read(attr_file)
 content.each_line{|d| elems=d.split(" "); a=Attr.new; a.name=elems[0]; a.meaning=elems[1]; a.save }
-puts "Loaded attr."
+puts "Done, loaded attr. Cost #{Time.now - start_time} seconds."
